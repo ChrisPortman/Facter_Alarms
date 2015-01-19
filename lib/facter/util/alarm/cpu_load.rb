98ef2a9d412883231142782edbf96566026ef59a
Chris Portman <chris.portman@optusnet.com.au>
2015-01-19 12:12:55 +1100

    Abstraction

---
+++ lib/facter/util/alarm/cpu_load.rb
@@ -0,0 +1,32 @@
+module Facter::Util
+  class Alarm::Cpu_load < Facter::Util::Alarm
+    def test
+      begin
+        cpu_load = File.read('/proc/loadavg')
+      rescue
+      end
+
+      num_cpus = Facter.value(:processorcount) || 1
+      cpu_load.split(/\s+/)[1].to_f
+    end
+
+    def state
+      begin
+        warning  = config['WARNING']
+        critical = config['CRITICAL']
+      rescue
+        warning  = 10
+        critical = 15
+      end
+      
+      case result
+      when > critical
+        'CRITICAL'
+      when > warning
+        'WARNING'
+      else
+        'OK'
+      end
+    end
+  end
+end
